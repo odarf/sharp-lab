ea1c587a530bd2162157836d8ce7daeabc0abfc1
odarf <70912176+odarf@users.noreply.github.com>
2021-06-29 11:53:42 +0300

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,84 @@
+﻿namespace WindowsFormsApp8._3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(371, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(202, 77);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "GetFieldClass";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(12, 12);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(353, 399);
+            this.richTextBox1.TabIndex = 1;
+            this.richTextBox1.Text = "";
+            // 
+            // richTextBox2
+            // 
+            this.richTextBox2.Location = new System.Drawing.Point(580, 12);
+            this.richTextBox2.Name = "richTextBox2";
+            this.richTextBox2.Size = new System.Drawing.Size(329, 399);
+            this.richTextBox2.TabIndex = 3;
+            this.richTextBox2.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(921, 473);
+            this.Controls.Add(this.richTextBox2);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.RichTextBox richTextBox2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace WindowsFormsApp8._3
+{
+    public partial class Form1 : Form
+    {
+        public OpenFileDialog openFileDialog = new OpenFileDialog
+        {
+            InitialDirectory = "C:\\Class1.cs",
+            Filter = @"cs files (*.cs)|*.cs|All files (*.*)|*.*"
+        };
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            GetFieldsClass(openFileDialog);
+        }
+
+        private void GetFieldsClass(OpenFileDialog openFileDialog)
+        {
+            int Number = 0;
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+                File.ReadAllText(openFileDialog.FileName);
+            StreamReader input = new StreamReader(openFileDialog.FileName);
+            List<string> ArrayString = new List<string>();
+            while (!input.EndOfStream)
+            {
+                ArrayString.Add(input.ReadLine());
+                richTextBox1.Text += ArrayString.Last<string>() + "\n";
+            }
+            for (int n = 0; n < ArrayString.Count; n++)
+            {
+                if (ArrayString[n].Contains("{"))
+                    Number++;
+                if (ArrayString[n].Contains("}"))
+                    Number--;
+                if (Number == 2)
+                if (((ArrayString[n].Contains(";") & !(ArrayString[n].Contains("(") | ArrayString[n].Contains(")"))) & (!ArrayString[n].Contains("using"))) | (ArrayString[n].Contains(";") & ArrayString[n].Contains("=")))
+                    richTextBox2.Text += ArrayString[n] + "\n";
+            }
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
